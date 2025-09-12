326a9ca9df5529924442416fa880c2b6c4aae445
Igor Babayan <igor.babayan@ukr.net>
2025-09-12 11:32:00 +0300

    UI fixes

--- EdAssistant/Views/SidebarNavigation.axaml.cs
+++ EdAssistant/Views/SidebarNavigation.axaml.cs
@@ -2,39 +2,35 @@ namespace EdAssistant.Views;
 
 public partial class SidebarNavigation : UserControl
 {
-    public static readonly RoutedEvent<NavigationEventArgs> NavigationRequestedEvent =
-        RoutedEvent.Register<SidebarNavigation, NavigationEventArgs>(
-            nameof(NavigationRequested), RoutingStrategies.Bubble);
-
-    public event EventHandler<NavigationEventArgs> NavigationRequested
-    {
-        add => AddHandler(NavigationRequestedEvent, value);
-        remove => RemoveHandler(NavigationRequestedEvent, value);
-    }
-
     private bool _isCollapsed;
     private Button _activeButton;
-
-    public bool IsCollapsed
-    {
-        get => _isCollapsed;
-        set
-        {
-            if (_isCollapsed != value)
-            {
-                _isCollapsed = value;
-                if (_isCollapsed)
-                    CollapseSidebar();
-                else
-                    ExpandSidebar();
-            }
-        }
-    }
 
     public SidebarNavigation()
     {
         InitializeComponent();
         _activeButton = this.FindControl<Button>("HomeBtn")!;
+
+        InitializeHeaderState();
+    }
+
+    private void InitializeHeaderState()
+    {
+        var expandedHeader = this.FindControl<Grid>("ExpandedHeader");
+        var collapsedButton = this.FindControl<Button>("CollapsedToggleButton");
+
+        if (expandedHeader is not null)
+            expandedHeader.IsVisible = true;
+        
+        if (collapsedButton is not null)
+            collapsedButton.IsVisible = false;
+
+        Width = 280;
+
+        var mainPanel = this.FindControl<StackPanel>("MainStackPanel");
+        if (mainPanel is not null)
+        {
+            mainPanel.Margin = new Thickness(20, 16);
+        }
     }
 
     private void ToggleButton_Click(object sender, RoutedEventArgs e)
@@ -54,6 +50,21 @@ public partial class SidebarNavigation : UserControl
     {
         Width = 60;
 
+        var headerBorder = this.FindControl<Border>("HeaderBorder");
+        if (headerBorder is not null)
+        {
+            headerBorder.Padding = new Thickness(12, 16);
+        }
+
+        var expandedHeader = this.FindControl<Grid>("ExpandedHeader");
+        var collapsedButton = this.FindControl<Button>("CollapsedToggleButton");
+
+        if (expandedHeader is not null)
+            expandedHeader.IsVisible = false;
+        
+        if (collapsedButton is not null)
+            collapsedButton.IsVisible = true;
+
         AddCollapsedClasses();
 
         var mainPanel = this.FindControl<StackPanel>("MainStackPanel");
@@ -67,6 +78,21 @@ public partial class SidebarNavigation : UserControl
     {
         Width = 280;
 
+        var headerBorder = this.FindControl<Border>("HeaderBorder");
+        if (headerBorder is not null)
+        {
+            headerBorder.Padding = new Thickness(16, 16, 8, 16);
+        }
+
+        var expandedHeader = this.FindControl<Grid>("ExpandedHeader");
+        var collapsedButton = this.FindControl<Button>("CollapsedToggleButton");
+
+        if (expandedHeader is not null)
+            expandedHeader.IsVisible = true;
+        
+        if (collapsedButton is not null)
+            collapsedButton.IsVisible = false;
+
         RemoveCollapsedClasses();
 
         var mainPanel = this.FindControl<StackPanel>("MainStackPanel");
@@ -78,12 +104,10 @@ public partial class SidebarNavigation : UserControl
 
     private void AddCollapsedClasses()
     {
-        var appTitle = this.FindControl<TextBlock>("AppTitle");
-        appTitle?.Classes.Add("collapsed");
-
         var sectionTitles = this.GetLogicalDescendants()
             .OfType<TextBlock>()
             .Where(tb => tb.Classes.Contains("section-title"));
+
         foreach (var title in sectionTitles)
         {
             title.Classes.Add("collapsed");
@@ -108,9 +132,6 @@ public partial class SidebarNavigation : UserControl
 
     private void RemoveCollapsedClasses()
     {
-        var appTitle = this.FindControl<TextBlock>("AppTitle");
-        appTitle?.Classes.Remove("collapsed");
-
         var sectionTitles = this.GetLogicalDescendants()
             .OfType<TextBlock>()
             .Where(tb => tb.Classes.Contains("section-title"));
@@ -144,16 +165,6 @@ public partial class SidebarNavigation : UserControl
 
             clickedButton.Classes.Add("active");
             _activeButton = clickedButton;
-
-            var navigationTarget = clickedButton.Tag!.ToString();
-
-            var args = new NavigationEventArgs(NavigationRequestedEvent, navigationTarget!);
-            RaiseEvent(args);
         }
     }
-}
-
-public class NavigationEventArgs(RoutedEvent routedEvent, string navigationTarget) : RoutedEventArgs(routedEvent)
-{
-    public string NavigationTarget { get; } = navigationTarget;
 }
