eb2f2d42ef1bc261caa0c69ebd3df6014e2e3791
Ihor Babaian <ihor.babaian@outlook.com>
2026-04-30 09:20:59 +0300

    Fix styles

--- ED.Assistant/Views/MainWindow.axaml.cs
+++ ED.Assistant/Views/MainWindow.axaml.cs
@@ -1,11 +1,46 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace ED.Assistant.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
+    public MainWindow() => InitializeComponent();
+
+    private void OnPointerPressed(object? sender, PointerPressedEventArgs args)
     {
-        InitializeComponent();
+        if (!args.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            return;
+
+        if (args.ClickCount == 2)
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+
+            return;
+        }
+
+        if (args.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+        {
+            BeginMoveDrag(args);
+        }
     }
+
+	private void OnMinimizeClick(object? sender, RoutedEventArgs args) => WindowState = WindowState.Minimized;
+
+	private void OnMaximizeClick(object? sender, RoutedEventArgs args) => WindowState = WindowState == WindowState.Maximized
+        ? WindowState.Normal
+        : WindowState.Maximized;
+
+	private void OnCloseClick(object? sender, RoutedEventArgs args)
+	{
+		if (Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+		{
+			desktop.TryShutdown();
+		}
+	}
 }
