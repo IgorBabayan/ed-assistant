7c1ecc019617e71bda91502370bafb32be25bb08
Ihor Babaian <ihor.babaian@outlook.com>
2026-05-03 19:59:24 +0300

    fix style for `ScanType`

---
+++ ED.Assistant/Converters/ScanTypeForegroundConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System.Globalization;
+
+namespace ED.Assistant.Converters;
+
+public sealed class ScanTypeForegroundConverter : IValueConverter
+{
+	public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+	{
+		var resources = Application.Current?.Resources;
+
+		if (resources is null)
+			return Brushes.White;
+
+		return value?.ToString() switch
+		{
+			"AutoScan" => resources["SecondaryTextBrush"] as IBrush ?? Brushes.Gray,
+			"Detailed" => resources["PrimaryAccentBrush"] as IBrush ?? Brushes.DeepSkyBlue,
+			_ => resources["PrimaryTextBrush"] as IBrush ?? Brushes.White
+		};
+	}
+
+	public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+		=> throw new NotSupportedException();
+}
